1faf781287af95d233c9f874b9520a2b724ae214
MacBook Air 13 <macbookair13@MacBooks-MacBook-Air.local>
2025-09-18 11:25:09 +0700

    update filter

--- src/components/filter-modal.tsx
+++ src/components/filter-modal.tsx
@@ -6,10 +6,16 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
-import { XIcon, FilterIcon } from "lucide-react";
+import {
+  XIcon,
+  FilterIcon,
+  CalendarIcon,
+  WalletIcon,
+  TagIcon,
+} from "lucide-react";
 import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
+
 import { TextField } from "./textField";
-import { WorkCaseIcon } from "@/assets/icons/outline";
 import { Button as SButton } from "./ui/button";
 
 interface ModalFilterProps {
@@ -19,8 +25,6 @@ interface ModalFilterProps {
 
 export function FilterModal({ open, setOpen }: ModalFilterProps) {
   const [tabs, setTabs] = useState(0);
-
-  const handleTabs = (index: number) => setTabs(index);
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
@@ -42,63 +46,76 @@ export function FilterModal({ open, setOpen }: ModalFilterProps) {
             </DialogDescription>
           </VisuallyHidden>
         </DialogHeader>
-        <div className="bg-white p-5 space-y-5 font-public">
-          <div className="grid grid-cols-4 gap-2">
-            <Button title="Rentan Waktu" onClick={() => handleTabs(1)} />
-            <Button title="Harian" onClick={() => handleTabs(2)} />
-            <Button title="Bulanan" onClick={() => handleTabs(3)} />
-            <Button title="Tahunan" onClick={() => handleTabs(4)} />
+        <div className="bg-white p-5 space-y-5 font-public relative overflow-auto md:overflow-hidden">
+          <div className="flex flex-nowrap justify-start gap-3 w-2xl">
+            <Button
+              title="Rentan Waktu"
+              tabs={tabs}
+              setTabs={setTabs}
+              number={1}
+            />
+            <Button title="Harian" tabs={tabs} setTabs={setTabs} number={2} />
+            <Button title="Bulanan" tabs={tabs} setTabs={setTabs} number={3} />
+            <Button title="Tahunan" tabs={tabs} setTabs={setTabs} number={4} />
+          </div>
+        </div>
+        <div className="grid grid-cols-2 gap-3 bg-white p-5">
+          {tabs === 1 ? (
+            <InputTime />
+          ) : tabs === 2 ? (
+            <InputDaily />
+          ) : tabs === 3 ? (
+            <InputMonthly />
+          ) : tabs === 4 ? (
+            <InputYears />
+          ) : null}
+
+          <div className="col-span-2 md:col-span-1">
+            <TextField
+              label="Akun yang digunakan"
+              placeholder="Atas nama Rekening"
+              icon={<WalletIcon className="size-4 text-primary" />}
+            />
+          </div>
+          <div className="col-span-2 md:col-span-1">
+            <TextField
+              label="Jenis Transaksi"
+              placeholder="Jenis Transaksi"
+              icon={<TagIcon className="size-4 text-primary" />}
+            />
           </div>
 
-          <div className="grid grid-cols-2 gap-3">
-            {tabs === 1 ? (
-              <InputTime />
-            ) : tabs === 2 ? (
-              <InputDaily />
-            ) : tabs === 3 ? (
-              <InputMonthly />
-            ) : tabs === 4 ? (
-              <InputYears />
-            ) : null}
-            <div className="col-span-1">
-              <TextField
-                label="Atas Nama"
-                placeholder="Atas nama Rekening"
-                icon={<WorkCaseIcon className="size-5 mr-1" color="#2B63E2" />}
-              />
-            </div>
-            <div className="col-span-1">
-              <TextField
-                label="Atas Nama"
-                placeholder="Atas nama Rekening"
-                icon={<WorkCaseIcon className="size-5 mr-1" color="#2B63E2" />}
-              />
-            </div>
-            <div className="col-span-2">
-              <TextField
-                label="Atas Nama"
-                placeholder="Atas nama Rekening"
-                icon={<WorkCaseIcon className="size-5 mr-1" color="#2B63E2" />}
-              />
-            </div>
-            <div className="col-span-1">
-              <TextField
-                label="Atas Nama"
-                placeholder="Atas nama Rekening"
-                icon={<WorkCaseIcon className="size-5 mr-1" color="#2B63E2" />}
-              />
-            </div>
-            <div className="col-span-1">
-              <TextField
-                label="Atas Nama"
-                placeholder="Atas nama Rekening"
-                icon={<WorkCaseIcon className="size-5 mr-1" color="#2B63E2" />}
-              />
-            </div>
+          <div className="col-span-2">
+            <TextField
+              label="Kategori"
+              placeholder="Katrgori"
+              icon={<TagIcon className="size-4 text-primary" />}
+            />
+          </div>
+
+          <div className="col-span-2 md:col-span-1">
+            <TextField
+              label="Nominal Minimum"
+              placeholder="0"
+              icon={
+                <span className="text-sm font-semibold text-primary">Rp.</span>
+              }
+            />
+          </div>
+          <div className="col-span-2 md:col-span-1">
+            <TextField
+              label="Nominal Maksimum"
+              placeholder="0"
+              icon={
+                <span className="text-sm font-semibold text-primary">Rp.</span>
+              }
+            />
           </div>
         </div>
         <div className="flex flex-row gap-4 justify-end items-center bg-blue-200 py-2 px-5 rounded-b-2xl">
-          <SButton className="rounded-full px-10 bg-transparent text-indigo-600">
+          <SButton
+            className="rounded-full px-10 bg-transparent text-indigo-600"
+            onClick={() => setOpen(false)}>
             Batal
           </SButton>
           <SButton className="rounded-full w-30 py-5 bg-blue-700 text-white">
@@ -113,12 +130,26 @@ export function FilterModal({ open, setOpen }: ModalFilterProps) {
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   title: string;
+  tabs: number;
+  setTabs: React.Dispatch<React.SetStateAction<number>>;
+  number: number;
 }
 
-function Button({ title, ...props }: ButtonProps) {
+function Button({ title, tabs, setTabs, number, ...props }: ButtonProps) {
+  const handleTabs = (index: number) => {
+    if (index === tabs) {
+      setTabs(0);
+      return;
+    }
+    setTabs(index);
+  };
+
   return (
     <button
-      className="bg-white rounded-full uppercase font-semibold border-2 border-blue-500 text-sm py-2"
+      onClick={() => handleTabs(number)}
+      className={`${
+        tabs === number ? "bg-blue-500 text-white" : ""
+      } rounded-full uppercase font-semibold border-2 border-blue-500 text-sm py-2 w-[150px] text-center`}
       {...props}>
       {title}
     </button>
@@ -128,18 +159,18 @@ function Button({ title, ...props }: ButtonProps) {
 function InputTime() {
   return (
     <>
-      <div className="col-span-1">
+      <div className="col-span-2 md:col-span-1">
         <TextField
-          label="Atas Nama"
-          placeholder="Atas nama Rekening"
-          icon={<WorkCaseIcon className="size-5 mr-1" color="#2B63E2" />}
+          label="Tanggal Awal"
+          placeholder="Tanggal Awal"
+          icon={<CalendarIcon className="size-4 text-primary" />}
         />
       </div>
-      <div className="col-span-1">
+      <div className="col-span-2 md:col-span-1">
         <TextField
-          label="Atas Nama"
-          placeholder="Atas nama Rekening"
-          icon={<WorkCaseIcon className="size-5 mr-1" color="#2B63E2" />}
+          label="Tanggal Akhir"
+          placeholder="Tanggal Akhir"
+          icon={<CalendarIcon className="size-4 text-primary" />}
         />
       </div>
     </>
@@ -148,44 +179,44 @@ function InputTime() {
 
 function InputDaily() {
   return (
+    <div className="col-span-2">
+      <TextField
+        label="Tanggal"
+        placeholder="Tanggal"
+        icon={<CalendarIcon className="size-4 text-primary" />}
+      />
+    </div>
+  );
+}
+
+function InputMonthly() {
+  return (
     <>
-      <div className="col-span-1">
+      <div className="col-span-2 md:col-span-1">
         <TextField
-          label="Atas Nama"
-          placeholder="Atas nama Rekening"
-          icon={<WorkCaseIcon className="size-5 mr-1" color="#2B63E2" />}
+          label="Bulan"
+          placeholder="Bulan"
+          icon={<CalendarIcon className="size-4 text-primary" />}
         />
       </div>
-      <div className="col-span-1">
+      <div className="col-span-2 md:col-span-1">
         <TextField
-          label="Atas Nama"
-          placeholder="Atas nama Rekening"
-          icon={<WorkCaseIcon className="size-5 mr-1" color="#2B63E2" />}
+          label="Tahun"
+          placeholder="Tahun"
+          icon={<CalendarIcon className="size-4 text-primary" />}
         />
       </div>
     </>
   );
 }
 
-function InputMonthly() {
-  return (
-    <div className="col-span-1">
-      <TextField
-        label="Atas Nama"
-        placeholder="Atas nama Rekening"
-        icon={<WorkCaseIcon className="size-5 mr-1" color="#2B63E2" />}
-      />
-    </div>
-  );
-}
-
 function InputYears() {
   return (
-    <div className="col-span-1">
+    <div className="col-span-2">
       <TextField
-        label="Atas Nama"
-        placeholder="Atas nama Rekening"
-        icon={<WorkCaseIcon className="size-5 mr-1" color="#2B63E2" />}
+        label="Tahun"
+        placeholder="Tahun"
+        icon={<CalendarIcon className="size-4 text-primary" />}
       />
     </div>
   );
